3bb556275af0ea5015402f2595a74eb09f3c1302
Iarlen <iarlensr@hotmail.com>
2023-08-18 23:25:39 -0300

    feat: finalizing the Home page, modifying globals.css and configuring next.config.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,28 @@
 import styles from './Home.module.css'
-export default function Home() {
-  return <h1 className={styles.main}>Hello World</h1>
+import { initialDrinks } from '@/utils/initialDrinks'
+import DrinkCard from '@/components/DrinkCard'
+import Search from '@/components/Search'
+
+export default async function Home() {
+  return (
+    <div className={styles.home}>
+      <div className={styles.information__container}>
+        <div className={styles.home__apresentation}>
+          <h1>I love Drinks</h1>
+          <p>Seu drink favorito se encontra aqui!</p>
+        </div>
+        <Search />
+      </div>
+      <div className={styles.drink__cards}>
+        {initialDrinks.map((drink) => (
+          <DrinkCard
+            key={drink.idDrink}
+            idDrink={drink.idDrink}
+            strDrinkThumb={drink.strDrinkThumb}
+            strDrink={drink.strDrink}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
